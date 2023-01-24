37cd7c75e43f56b66b318003a9b7504b4a7e50ec
eliashz <eliashz@gmail.com>
2023-01-24 10:56:39 +0100

    redux wip

--- src/store/houses.slice.js
+++ src/store/houses.slice.js
@@ -1,11 +1,17 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { urls } from '../constants'
 
-export const getHouses = createAsyncThunk('houses/getHouses', async () => {
-  const res = await fetch(urls.houses)
-  const data = await res.json()
-  return data
-})
+export const getHouses = createAsyncThunk(
+  'houses/getHouses',
+  async (name, { rejectWithValue }) => {
+    const res = await fetch(urls.houses)
+    const data = await res.json()
+    if (res.status < 200 || res.status >= 300) {
+      return rejectWithValue(data)
+    }
+    return data
+  },
+)
 
 const initialState = {
   reqStatus: 'initial',
@@ -22,27 +28,28 @@ const housesSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getHouses.pending, (state) => {
-      state.reqStatus = 'loading'
-    })
-    builder.addCase(getHouses.fulfilled, (state, action) => {
-      state.reqStatus = 'success'
-      action.payload.forEach((house) => {
-        state.houses.byId[house.id] = house
-        if (!state.houses.allIds.includes(house.id)) {
-          state.houses.allIds.push(house.id)
-        }
-        if (!state.houses.byCity.includes(house.city)) {
-          state.houses.byCity.push(house.city)
-        }
-        if (!state.houses.byType.includes(house.type)) {
-          state.houses.byType.push(house.type)
-        }
+    builder
+      .addCase(getHouses.pending, (state) => {
+        state.reqStatus = 'loading'
       })
-    })
-    builder.addCase(getHouses.rejected, (state) => {
-      state.reqStatus = 'failed'
-    })
+      .addCase(getHouses.fulfilled, (state, action) => {
+        state.reqStatus = 'success'
+        action.payload.forEach((house) => {
+          state.houses.byId[house.id] = house
+          if (!state.houses.allIds.includes(house.id)) {
+            state.houses.allIds.push(house.id)
+          }
+          if (!state.houses.byCity.includes(house.city)) {
+            state.houses.byCity.push(house.city)
+          }
+          if (!state.houses.byType.includes(house.type)) {
+            state.houses.byType.push(house.type)
+          }
+        })
+      })
+      .addCase(getHouses.rejected, (state) => {
+        state.reqStatus = 'failed'
+      })
   },
 })
 
